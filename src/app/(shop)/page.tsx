fceb5cb507b132839e666e17eb7d16266c3270a1
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-10-23 13:47:10 -0500

    Componente Title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,13 @@
-import { titleFont } from "@/config/fonts";
+import { Title } from "@/components";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hola mundo</h1>
-      <h1 className={titleFont.className}>Hola mundo</h1>
-    </div>
+    <>
+      <Title
+        title={"Tienda"}
+        subTitle="Todos los productos"
+        className="mb-2"
+      />
+    </>
   );
 }
