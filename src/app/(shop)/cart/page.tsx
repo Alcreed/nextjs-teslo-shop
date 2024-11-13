9210bc4bf828475ef630cb195cf6dee87f15658b
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-11-13 16:51:15 -0500

    Página carro de compras vacío

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -3,6 +3,7 @@ import { initialData } from "@/seed/seed";
 
 import { QuantitySelector, Title } from "@/components";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 const productsInCart = [
   initialData.products[0],
@@ -11,6 +12,11 @@ const productsInCart = [
 ]
 
 export default function CartPage() {
+
+  if (productsInCart.length === 0) {
+    redirect("/empty")
+  }
+
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
