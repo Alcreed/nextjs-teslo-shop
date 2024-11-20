281ad50d7828f54ba1db177051d9986abcf133b9
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-11-20 14:03:21 -0500

    Insertando productos e imagenes

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -20,6 +20,36 @@ async function main() {
     data: categoriesData,
   });
 
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+
+    return map;
+  }, {} as Record<string, string>);
+
+  // Productos
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // Images
+    const imagesData = await images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
   console.log("Seed ejecutado correctamente");
 }
 
