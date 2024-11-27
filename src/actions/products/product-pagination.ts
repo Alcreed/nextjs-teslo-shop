5b8e9a626abda1111e3593ff98efe6fd02142c8e
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-11-27 12:29:35 -0500

    Paginación - determinar número de páginas

--- src/actions/products/product-pagination.ts
+++ src/actions/products/product-pagination.ts
@@ -2,10 +2,23 @@
 
 import prisma from "@/lib/prisma";
 
-export const getPaginatedProductsWithImages = async () => {
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
   try {
+    // 1. Obtener productos
     const products = await prisma.product.findMany({
-      // take: 3,
+      take: take,
+      skip: (page - 1) * take,
       include: {
         ProductImage: {
           take: 2,
@@ -16,9 +29,14 @@ export const getPaginatedProductsWithImages = async () => {
       },
     });
 
+    // 2. Obtener total de páginas
+    // todo
+    const totalCount = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCount / take);
+
     return {
-      currentPage: 1,
-      totalPages: 10,
+      currentPage: page,
+      totalPages: totalPages,
       products: products.map((product) => ({
         ...product,
         images: product.ProductImage.map((image) => image.url),
